c054e9502ea0ac8ae93a21a98d146482aa9f99cc
jQuirozMondaca <128020518+jQuirozMondaca@users.noreply.github.com>
2024-03-05 21:44:26 -0300

    terminado

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+precio = 400000;
+cantidad = 1;
+
+precioSpan = document.querySelector(".precio");
+precioSpan.innerHTML = precio;
+
+cantidadSpan = document.querySelector(".cantidad")
+cantidadSpan.innerHTML = cantidad;
+
+valorSpan = document.querySelector(".valor-total");
+valorSpan.innerHTML = precio;
